0648f121dc3d5f6d19b8d11fd76f14d607012656
Lucas <lucas@tripintl.com>
2022-10-21 01:06:13 +0800

    feat(admin): add pages and tags translation

--- ghost/admin/app/controllers/pages.js
+++ ghost/admin/app/controllers/pages.js
@@ -2,39 +2,60 @@ import PostsController from './posts';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
 
-const TYPES = [{
-    name: 'All pages',
-    value: null
-}, {
-    name: 'Draft pages',
-    value: 'draft'
-}, {
-    name: 'Published pages',
-    value: 'published'
-}, {
-    name: 'Scheduled pages',
-    value: 'scheduled'
-}, {
-    name: 'Featured pages',
-    value: 'featured'
-}];
+const TYPES = [
+    {
+        name: 'All pages',
+        value: null
+    },
+    {
+        name: 'Draft pages',
+        value: 'draft'
+    },
+    {
+        name: 'Published pages',
+        value: 'published'
+    },
+    {
+        name: 'Scheduled pages',
+        value: 'scheduled'
+    },
+    {
+        name: 'Featured pages',
+        value: 'featured'
+    }
+];
 
-const ORDERS = [{
-    name: 'Newest first',
-    value: null
-}, {
-    name: 'Oldest first',
-    value: 'published_at asc'
-}, {
-    name: 'Recently updated',
-    value: 'updated_at desc'
-}];
+const ORDERS = [
+    {
+        name: 'Newest first',
+        value: null
+    },
+    {
+        name: 'Oldest first',
+        value: 'published_at asc'
+    },
+    {
+        name: 'Recently updated',
+        value: 'updated_at desc'
+    }
+];
 
 export default class PagesController extends PostsController {
     @service router;
+    @service intl;
 
-    availableTypes = TYPES;
-    availableOrders = ORDERS;
+    availableTypes = TYPES.map((_) => {
+        return {
+            ..._,
+            name: this.intl.t(`Manual.Pages.${_.name.replace(/\s/g, '_')}`)
+        };
+    });
+    availableOrders = ORDERS.map((_) => {
+        return {
+            ..._,
+            name: this.intl.t(`Manual.Pages.${_.name.replace(/\s/g, '_')}`)
+        };
+    });
 
     @action
     openEditor(page) {
