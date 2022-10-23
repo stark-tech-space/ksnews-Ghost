4ef98c27df6eab76af2aba180570ffbd6981a49f
Lucas <lucas@tripintl.com>
2022-10-23 22:40:52 +0800

    feat(admin): add recipients select translation

--- ghost/admin/app/components/settings/members-email/default-recipients-select.js
+++ ghost/admin/app/components/settings/members-email/default-recipients-select.js
@@ -1,43 +1,58 @@
 import Component from '@glimmer/component';
 import {action} from '@ember/object';
+import {inject as service} from '@ember/service';
 
 export default class SettingsDefaultEmailRecipientsComponent extends Component {
+    @service intl;
+
     get options() {
-        return [{
-            name: 'Whoever has access to the post',
-            description: 'Free posts to everyone, premium posts sent to paid members',
-            value: 'visibility',
-            icon: 'members-post',
-            icon_color: 'green'
-        }, {
-            name: 'All members',
-            description: 'Everyone who is subscribed to newsletter updates, whether free or paid members',
-            value: 'all-members',
-            icon: 'members-all',
-            icon_color: 'blue'
-        }, {
-            name: 'Paid-members only',
-            description: 'People who have a premium subscription',
-            value: 'paid-only',
-            icon: 'members-paid',
-            icon_color: 'pink'
-        }, {
-            name: 'Specific people',
-            description: 'Only people with any of the selected tiers or labels',
-            value: 'segment',
-            icon: 'members-segment',
-            icon_color: 'yellow'
-        }, {
-            name: 'Usually nobody',
-            description: 'Newsletters are off for new posts, but can be enabled when needed',
-            value: 'none',
-            icon: 'no-members',
-            icon_color: 'midlightgrey-d2'
-        }];
+        return [
+            {
+                name: 'Whoever has access to the post',
+                description: 'Free posts to everyone, premium posts sent to paid members',
+                value: 'visibility',
+                icon: 'members-post',
+                icon_color: 'green'
+            },
+            {
+                name: 'All members',
+                description: 'Everyone who is subscribed to newsletter updates, whether free or paid members',
+                value: 'all-members',
+                icon: 'members-all',
+                icon_color: 'blue'
+            },
+            {
+                name: 'Paid-members only',
+                description: 'People who have a premium subscription',
+                value: 'paid-only',
+                icon: 'members-paid',
+                icon_color: 'pink'
+            },
+            {
+                name: 'Specific people',
+                description: 'Only people with any of the selected tiers or labels',
+                value: 'segment',
+                icon: 'members-segment',
+                icon_color: 'yellow'
+            },
+            {
+                name: 'Usually nobody',
+                description: 'Newsletters are off for new posts, but can be enabled when needed',
+                value: 'none',
+                icon: 'no-members',
+                icon_color: 'midlightgrey-d2'
+            }
+        ].map((_) => {
+            return {
+                ..._,
+                name: this.intl.t(`Manual.Settings.${_.name.replace(/ /g, '_')}`),
+                description: this.intl.t(`Manual.Settings.${_.description.replace(/ /g, '_')}`)
+            };
+        });
     }
 
     get selectedOption() {
-        return this.options.find(o => o.value === this.args.recipients);
+        return this.options.find((o) => o.value === this.args.recipients);
     }
 
     @action
